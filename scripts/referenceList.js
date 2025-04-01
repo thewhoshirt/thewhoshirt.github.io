accfb6981300b9f1563ac27be2a5c9f2f581c6c5
janelle-and <janelle.andrews@rdpolytech.ca>
2025-04-01 14:05:41 -0600

    reference list js

---
+++ scripts/referenceList.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    // sets page theme
+    var currentTheme = localStorage.getItem("theme");
+    if (currentTheme == null){
+        localStorage.setItem("theme","dark")
+    };
+    if (currentTheme == "light"){
+        $("body").css("backgroundColor","#f2f2f2")
+    };
+
+    // toggles page theme with the button
+    $("#theme").click(function(){
+        currentTheme = localStorage.getItem("theme");
+        if (currentTheme == "dark"){
+            currentTheme = localStorage.setItem("theme","light");
+            $("body").css("backgroundColor","#f2f2f2")
+        }
+        else{
+            currentTheme = localStorage.setItem("theme","dark");
+            $("body").css("backgroundColor","slategray")
+        };
+    });
+});
