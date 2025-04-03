6c943edb3708239bd11581b5945eb5b3e179e755
janelle-and <janelle.andrews@rdpolytech.ca>
2025-04-02 22:36:30 -0600

    youtube videos with api

--- scripts/experience.js
+++ scripts/experience.js
@@ -23,4 +23,24 @@ $(document).ready( function() {
 
   // creates the accordion sections
     $( "#accordion" ).accordion();
-  } );
+
+    var apikey = "AIzaSyBygxWHhXyl-m3c3fGEhQzpWtCbDvlmdgQ";
+    var url = `https://www.googleapis.com/youtube/v3/search?part=snippet&q=Microsoft&maxResults=5&order=relevance&type=video&key=${apikey}`
+    $.get(url, function(data){
+        if(data.items && data.items.length > 0){
+            var videoHtml = "";
+            $.each(data.items, function(index,video){
+                var videoId = video.id.videoId;
+                var thumbnail = video.snippet.thumbnails.high.url;
+                videoHtml += 
+                    `<a href="https://www.youtube.com/watch?v=${videoId}" target="_blank">
+                        <img src="${thumbnail}">    
+                    </a>`
+            });
+            $("#videos").html(videoHtml);
+        }
+        else{
+            $("#videos").html("<p>No videos found.</p>")
+        }
+    })
+});
