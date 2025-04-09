5ad2fa954e5196ae137b7ce81ce3523295d4cf9e
thewhoshirt <s.embury.36@gmail.com>
2025-04-09 12:09:45 -0600

    Fixed knife darkmode

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,14 @@ $(document).ready(function(){
     // sets page theme
     var currentTheme = localStorage.getItem("theme");
     if (currentTheme == null){
-        localStorage.setItem("theme","dark");
+        localStorage.setItem("theme","light");
         $("#lightMode").css("display","none");
         $("#darkMode").css("display","block");
     };
-    if (currentTheme == "light"){
-        $("body").css("backgroundColor","#f5f9dc");
-        $("#lightMode").css("display","block");
-        $("#darkMode").css("display","none");
+    if (currentTheme == "dark"){
+        $("body").css({"backgroundColor":" #060c04","color":" #f9d864"});
+        $("#darkMode").css("display","block");
+        $("#lightMode").css("display","none");
     };
 
     // toggles page theme with the button
@@ -18,14 +18,14 @@ $(document).ready(function(){
         if (currentTheme == "light"){
             currentTheme = localStorage.setItem("theme","dark");
             $("body").css({"backgroundColor":" #060c04","color":" #f9d864"});
-           $("#lightMode").css("display","block");
-           $("#darkMode").css("display","none");
+           $("#darkMode").css("display","block");
+           $("#lightMode").css("display","none");
         }
         else{
             currentTheme = localStorage.setItem("theme","light");
             $("body").css({"backgroundColor":" #f5f9dc","color":" #112f08"})
-            $("#lightMode").css("display","none");
-            $("#darkMode").css("display","block");
+            $("#darkMode").css("display","none");
+            $("#lightMode").css("display","block");
         }
     });
 
