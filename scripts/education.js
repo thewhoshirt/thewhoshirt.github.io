c6434c2d70e5f8e1c947504045cc8bf9492db0e6
thewhoshirt <s.embury.36@gmail.com>
2025-04-09 11:43:39 -0600

    Theme Update, Not all pages.

--- scripts/education.js
+++ scripts/education.js
@@ -5,19 +5,19 @@ $(document).ready(function(){
         localStorage.setItem("theme","dark")
     }
     if (currentTheme == "light"){
-        $("body").css("backgroundColor","#f2f2f2")
+        $("body").css("backgroundColor","#f5f9dc")
     }
 
     // toggles page theme with the button
     $("#theme").click(function(){
         currentTheme = localStorage.getItem("theme");
-        if (currentTheme == "dark"){
-            currentTheme = localStorage.setItem("theme","light");
-            $("body").css("backgroundColor","#f2f2f2")
+        if (currentTheme == "light"){
+            currentTheme = localStorage.setItem("theme","dark");
+            $("body").css({"backgroundColor":" #060c04","color":" #f9d864"})
         }
         else{
-            currentTheme = localStorage.setItem("theme","dark");
-            $("body").css("backgroundColor","slategray")
+            currentTheme = localStorage.setItem("theme","light");
+            $("body").css({"backgroundColor":" #f5f9dc","color":" #112f08"})
         }
     });
 
