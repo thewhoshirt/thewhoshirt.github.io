03017ec4f4fbd749acda122856cac712933a98fc
janelle-and <janelle.andrews@rdpolytech.ca>
2025-04-09 20:46:42 -0600

    fix whisk

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -6,8 +6,8 @@ $(document).ready(function(){
     };
     if (currentTheme == "dark"){
         $("body").css({"backgroundColor":" #060c04","color":" #f9d864"});
-        $("#darkMode").css("display","none");
-        $("#lightMode").css("display","block");
+        $("#darkMode").css("display","block");
+        $("#lightMode").css("display","none");
     };
 
     // toggles page theme with the button
@@ -16,14 +16,14 @@ $(document).ready(function(){
         if (currentTheme == "light"){
             currentTheme = localStorage.setItem("theme","dark");
             $("body").css({"backgroundColor":" #060c04","color":" #f9d864"})
-            $("#darkMode").css("display","none");
-            $("#lightMode").css("display","block");
+            $("#darkMode").css("display","block");
+            $("#lightMode").css("display","none");
         }
         else{
             currentTheme = localStorage.setItem("theme","light");
             $("body").css({"backgroundColor":" #f5f9dc","color":" #112f08"});
-            $("#darkMode").css("display","block");
-            $("#lightMode").css("display","none");
+            $("#darkMode").css("display","none");
+            $("#lightMode").css("display","block");
         };
     });
 
@@ -38,18 +38,7 @@ $(document).ready(function(){
             .animate({ borderSpacing: 0 }, {step: function(now,fx) {
                     $(this).css('transform','rotate('+now+'deg)');}, duration:500},'linear')
     });
-    $(".whisk").click(function animateTitle(){
-        $(".whisk").animate({borderSpacing: +100 }, {
-            step: function(now, fx) {$(this).css('transform','rotate('+now+'deg)');},duration:500},'linear')
-            .animate({ borderSpacing: 0 }, {step: function(now,fx) {
-              $(this).css('transform','rotate('+now+'deg)');   }, duration:500},'linear')
-            .animate({ borderSpacing: +100 }, {step: function(now,fx) {
-                $(this).css('transform','rotate('+now+'deg)');   }, duration:500},'linear')
-            .animate({ borderSpacing: 0 }, {step: function(now,fx) {
-                    $(this).css('transform','rotate('+now+'deg)');   }, duration:500},'linear')
-            .animate({ borderSpacing: 0 }, {step: function(now,fx) {
-                $(this).css('transform','rotate('+now+'deg)');   }, duration:500},'linear')
-    });
+
     // contact form validation
     $("#contactForm").validate({
         rules:{
